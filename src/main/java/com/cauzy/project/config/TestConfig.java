993c456c0fa3ff4b0875cdc78b44b16ede91ae4b
Cauã Farias <cauafariasdev@gmail.com>
2024-04-22 09:24:38 -0300

    feat: add payment

--- src/main/java/com/cauzy/project/config/TestConfig.java
+++ src/main/java/com/cauzy/project/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.cauzy.project.entities.Category;
 import com.cauzy.project.entities.Order;
 import com.cauzy.project.entities.OrderItem;
+import com.cauzy.project.entities.Payment;
 import com.cauzy.project.entities.Product;
 import com.cauzy.project.entities.User;
 import com.cauzy.project.entities.enums.OrderStatus;
@@ -81,7 +82,11 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
-//		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+
 	}
 	
 	
