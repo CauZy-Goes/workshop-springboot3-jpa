f1fda6d03aa941014ec614816d41b62180e798e5
Cauã Farias <cauafariasdev@gmail.com>
2024-04-22 11:28:21 -0300

    feat: add Exception handling findById

--- src/main/java/com/cauzy/project/resources/UserResource.java
+++ src/main/java/com/cauzy/project/resources/UserResource.java
@@ -3,7 +3,6 @@ package com.cauzy.project.resources;
 import java.net.URI;
 import java.util.List;
 
-import org.slf4j.helpers.Reporter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
