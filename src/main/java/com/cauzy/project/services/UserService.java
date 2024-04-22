234482a0fe044d24159055c297b8f5183a50a8fd
Cauã Farias <cauafariasdev@gmail.com>
2024-04-22 11:56:31 -0300

    feat: add Exception handling update

--- src/main/java/com/cauzy/project/services/UserService.java
+++ src/main/java/com/cauzy/project/services/UserService.java
@@ -12,6 +12,8 @@ import com.cauzy.project.repositories.UserRepository;
 import com.cauzy.project.services.exceptions.DatabaseException;
 import com.cauzy.project.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service // diz que um coponente spring, assim o Autowired funciona
 public class UserService {
 
@@ -45,9 +47,13 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
