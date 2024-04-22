74585d5a367e05b5824d95e4cbf1bf8c8e4d4f34
Cauã Farias <cauafariasdev@gmail.com>
2024-04-22 10:32:40 -0300

    feat: add user Update

--- src/main/java/com/cauzy/project/resources/UserResource.java
+++ src/main/java/com/cauzy/project/resources/UserResource.java
@@ -3,12 +3,14 @@ package com.cauzy.project.resources;
 import java.net.URI;
 import java.util.List;
 
+import org.slf4j.helpers.Reporter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,5 +49,11 @@ public class UserResource {
 	public ResponseEntity<Void> delete(@PathVariable Long id){
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj){
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
 	}
 }
